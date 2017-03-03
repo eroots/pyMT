300200c0bdd0091bf1e771c0097d31152f89e825
Eric Roots <eroots087@gmail.com>
2017-03-03 15:26:43 -0500

    new folders

---
+++ GUI_common/common_functions.py
@@ -0,0 +1,86 @@
+from PyQt4 import QtCore, QtGui
+import pyMT.utils as utils
+
+
+def check_key_presses(key_presses, verbose=False):
+    msg = ''
+    retval = {'Control': False, 'Shift': False, 'Alt': False}
+    if key_presses == QtCore.Qt.ShiftModifier:
+        retval['Shift'] = True
+    elif key_presses == QtCore.Qt.ControlModifier:
+        retval['Control'] = True
+    elif key_presses == QtCore.Qt.AltModifier:
+        retval['Alt'] = True
+    elif key_presses == (QtCore.Qt.ControlModifier |
+                         QtCore.Qt.ShiftModifier):
+        retval['Shift'] = True
+        retval['Control'] = True
+    elif key_presses == (QtCore.Qt.AltModifier |
+                         QtCore.Qt.ShiftModifier):
+        retval['Alt'] = True
+        retval['Shift'] = True
+    elif key_presses == (QtCore.Qt.ControlModifier |
+                         QtCore.Qt.AltModifier):
+        retval['Control'] = True
+        retval['Alt'] = True
+    elif key_presses == (QtCore.Qt.ControlModifier |
+                         QtCore.Qt.ShiftModifier |
+                         QtCore.Qt.AltModifier):
+        retval['Shift'] = True
+        retval['Alt'] = True
+        retval['Control'] = True
+    if any(retval.values()):
+        msg = '+'.join([k for k in retval if retval[k]])
+        msg = msg.replace('Shift', 'Periods')
+        msg = msg.replace('Control', 'Components')
+        msg = msg.replace('Alt', 'Sites')
+    if verbose and msg:
+        print(' '.join(['Editting all', msg]))
+    return retval
+
+
+class FileDialog(QtGui.QInputDialog):
+    def __init__(self, parent=None):
+        super(FileDialog, self).__init__(parent)
+        self.accepted = QtGui.QPushButton('OK')
+        self.rejected = QtGui.QPushButton('Cancel')
+        # self.accepted.connect(self.accept)
+        # self.rejected.connect(self.reject)
+
+    @staticmethod
+    def write_file(parent=None, default=None, label='Input'):
+        while True:
+            dialog = FileDialog(parent)
+            dialog.setTextValue(default)
+            dialog.setLabelText(label)
+            ret = dialog.exec_()
+            file = dialog.textValue()
+            if ret and file:
+                if utils.check_file(file):
+                    reply = QtGui.QMessageBox.question(parent, 'Message',
+                                                       'File already exists. Overwrite?',
+                                                       QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+                    if reply == QtGui.QMessageBox.Yes:
+                        return file, ret
+                else:
+                    return file, ret
+            else:
+                return file, 0
+
+    @staticmethod
+    def read_file(parent=None, default=None, label='Output'):
+        while True:
+            dialog = FileDialog(parent)
+            dialog.setTextValue(default)
+            dialog.setLabelText(label)
+            ret = dialog.exec_()
+            file = dialog.textValue()
+            if ret and file:
+                if not utils.check_file(file):
+                    QtGui.QMessageBox.about(parent, 'Message',
+                                            'File not found')
+                else:
+                    return file, ret
+            else:
+                return file, 0
+
---
+++ ModelGUI/mesh_designer.py
@@ -0,0 +1,238 @@
+# License: Creative Commons Zero (almost public domain) http://scpyce.org/cc0
+
+"""
+Example usage of matplotlibs widgets: Build a small 2d Data viewer.
+
+Shows 2d-data as a pcolormesh, a click on the image shows the crossection
+(x or y, depending on the mouse button) and draws a corresponding line in
+the image, showing the location of the crossections. A reset button deletes all
+crossections plots.
+
+Works with matplotlib 1.0.1.
+"""
+
+from matplotlib.widgets import Cursor
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+from PyQt4.uic import loadUiType
+from PyQt4 import QtGui
+from pyMT.GUI_common.common_functions import check_key_presses, FileDialog
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar)
+import sys
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+Ui_MainWindow, QMainWindow = loadUiType(os.path.join(path, 'mesh_designer.ui'))
+plt.rcParams['font.size'] = 8
+
+
+class model_viewer_2d(QMainWindow, Ui_MainWindow):
+    def __init__(self, model, data=None):
+        super(model_viewer_2d, self).__init__()
+        """
+        Shows a given array in a 2d-viewer.
+        Input: z, an 2d array.
+        x,y coordinters are optional.
+        """
+        if data:
+            self.site_locations = data.locations
+        else:
+            self.site_locations = []
+            self.regenMesh_2.setEnabled(False)
+        self.setupUi(self)
+        self.orientation = 'xy'
+        self.site_marker = 'w+'
+        self.mesh_color = 'w'
+        self.slice_idx = 0
+        self.mesh_changable = True
+        self.fig = Figure()
+        self.key_presses = {'Control': False, 'Alt': False, 'Shift': False}
+        # self.fig = plt.figure()
+        self.cid = {'Mesh': []}
+        self.model = copy.deepcopy(model)
+        self.file_dialog = FileDialog(self)
+        self.revert_model = copy.deepcopy(model)
+        self.overview = self.fig.add_subplot(111)
+        self.delete_tolerance = 0.5
+        self.overview.autoscale(1, 'both', 1)
+        self.addmpl(self.fig)
+        self.redraw_pcolor()
+        # self.overview.pcolor(self.model.dy, self.model.dx,
+        #                      self.model.vals[:, :, self.slice_idx], edgecolors='w', picker=3)
+        cursor = Cursor(self.overview, useblit=True, color='black', linewidth=2)
+        self._widgets = [cursor]
+        self.connect_mpl_events()
+        self.setup_widgets()
+        self.minX.setText(str(min(self.model.xCS)))
+        self.minY.setText(str(min(self.model.yCS)))
+        self.maxX.setText(str(max(self.model.xCS)))
+        self.maxY.setText(str(max(self.model.yCS)))
+
+    def setup_widgets(self):
+        self.writeModel.triggered.connect(self.write_model)
+        self.saveProgress.triggered.connect(self.save_progress)
+        self.revertProgress.triggered.connect(self.revert_progress)
+        self.regenMesh_2.clicked.connect(self.regenerate_mesh)
+        self.addPad.clicked.connect(self.add_pads)
+        self.removePad.clicked.connect(self.remove_pads)
+
+    def add_pads(self):
+        print('Adding pads')
+        xmesh = self.model.xCS
+        ymesh = self.model.yCS
+        if self.padLeft.checkState():
+            pad = self.padMult.value() * ymesh[0]
+            self.model.dy_insert(0, self.model.dy[0] - pad)
+        if self.padRight.checkState():
+            pad = self.padMult.value() * ymesh[-1]
+            self.model.dy_insert(self.model.ny + 1, pad + self.model.dy[-1])
+        if self.padTop.checkState():
+            pad = self.padMult.value() * xmesh[-1]
+            self.model.dx_insert(self.model.nx + 1, self.model.dx[-1] + pad)
+        if self.padBottom.checkState():
+            pad = self.padMult.value() * xmesh[0]
+            self.model.dx_insert(0, self.model.dx[0] - pad)
+        self.redraw_pcolor()
+
+    def remove_pads(self):
+        print('Removing pads')
+        if self.padLeft.checkState():
+            self.model.dy_delete(0)
+        if self.padRight.checkState():
+            self.model.dy_delete(self.model.nx)
+        if self.padBottom.checkState():
+            self.model.dx_delete(0)
+        if self.padTop.checkState():
+            self.model.dx_delete(self.model.nx)
+        self.redraw_pcolor()
+
+    def regenerate_mesh(self):
+        print('Regenerating mesh')
+        self.model.generate_mesh(self.site_locations,
+                                 min_x=float(self.minX.text()),
+                                 min_y=float(self.minY.text()),
+                                 max_x=float(self.maxX.text()),
+                                 max_y=float(self.maxY.text()))
+        self.redraw_pcolor()
+
+    def revert_progress(self):
+        print('Reverting...')
+        self.model = copy.deepcopy(self.revert_model)
+        self.redraw_pcolor()
+
+    def save_progress(self):
+        self.revert_model = copy.deepcopy(self.model)
+
+    def write_model(self):
+        file, ret = self.file_dialog.write_file()
+        if ret:
+            self.model.write(file)
+
+    def connect_mpl_events(self):
+        if self.mesh_changable:
+            print('Trying to connect')
+            self.cid['Mesh'] = self.canvas.mpl_connect('button_release_event', self.click)
+
+    def addmpl(self, fig):
+        self.canvas = FigureCanvas(fig)  # Make a canvas
+        self.mplvl.addWidget(self.canvas)
+        # self.canvas.setParent(self.mplwindow)
+        # self.canvas.setFocusPolicy(QtCore.Qt.ClickFocus)
+        # self.canvas.setFocus()
+        self.toolbar = NavigationToolbar(canvas=self.canvas,
+                                         parent=self.mplwindow, coordinates=True)
+        # Connect check box to instance
+        self.canvas.draw()
+        self.mplvl.addWidget(self.toolbar)
+
+    def show_legend(self, event):
+        """Shows legend for the plots"""
+        print('NX: {}, NY: {}, NZ: {}'.format(self.model.nx, self.model.ny, self.model.nz))
+        # for pl in [self.x_subplot, self.y_subplot]:
+        #     if len(pl.lines) > 0:
+        #         pl.legend()
+        # plt.draw()
+
+    def clear_xy_subplots(self, event):
+        """Clears the subplots."""
+        self.model = copy.deepcopy(self.revert_model)
+        self.canvas.draw()
+
+    def redraw_pcolor(self):
+        self.overview.clear()
+        self.overview.autoscale(1, 'both', 1)
+        self.overview.pcolor(self.model.dy, self.model.dx,
+                             self.model.vals[:, :, self.slice_idx], edgecolors=self.mesh_color, picker=3)
+        if np.any(self.site_locations):
+            self.overview.plot(self.site_locations[:, 1],
+                               self.site_locations[:, 0],
+                               self.site_marker)
+        self.canvas.draw()
+
+    def click(self, event):
+        """
+        What to do, if a click on the figure happens:
+            1. Check which axis
+            2. Get data coord's.
+            3. Plot resulting data.
+            4. Update Figure
+        """
+        self.key_presses = check_key_presses(QtGui.QApplication.keyboardModifiers())
+        # print(key_presses)
+        # Get nearest data
+        xpos = np.argmin(np.abs(event.xdata - self.model.dy))
+        ypos = np.argmin(np.abs(event.ydata - self.model.dx))
+        # Check which mouse button:
+        if event.button == 1:
+            # Plot it
+            if event.xdata > self.model.dy[xpos]:
+                xpos += 1
+            if self.key_presses['Control']:
+                diff = np.abs(event.xdata - self.model.dy[xpos])
+                print(diff)
+                print(self.delete_tolerance * (self.model.dy[xpos] - self.model.dy[xpos - 1]))
+                if diff <= self.delete_tolerance * (self.model.dy[xpos] - self.model.dy[xpos - 1]):
+                    self.model.dy_delete(xpos)
+            else:
+                self.model.dy_insert(xpos, event.xdata)
+        elif event.button == 3:
+            if event.ydata > self.model.dx[ypos]:
+                ypos += 1
+            if self.key_presses['Control']:
+                diff = np.abs(event.ydata - self.model.dx[ypos])
+                print(diff)
+                print(self.delete_tolerance * (self.model.dx[ypos] - self.model.dx[ypos - 1]))
+                if diff <= self.delete_tolerance * (self.model.dx[ypos] - self.model.dx[ypos - 1]):
+                    self.model.dx_delete(ypos)
+            else:
+                self.model.dx_insert(ypos, event.ydata)
+        self.redraw_pcolor()
+        # Show it
+
+    def show_plot(self):
+        pass
+
+
+if __name__ == '__main__':
+    # Build some strange looking data:
+    x = np.linspace(-3, 3, 300)
+    y = np.linspace(-4, 4, 400)
+    X, Y = np.meshgrid(x, y)
+    z = np.sqrt(X**2 + Y**2) + np.sin(X**2 + Y**2)
+    w, h = 512, 512
+    from matplotlib import cbook
+    datafile = cbook.get_sample_data('ct.raw', asfileobj=False)
+    print('loading', datafile)
+    s = open(datafile, 'rb').read()
+    A = np.fromstring(s, np.uint16).astype(float)
+    A *= 1.0 / max(A)
+    A.shape = w, h
+    # Put it in the viewer
+    fig_v = model_viewer_2d(z, x, y)
+    fig_v2 = model_viewer_2d(A)
+    # Show it
+    plt.show()
