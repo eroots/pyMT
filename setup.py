4d8e79ef0740c960b58c9e7a1ff536951a61e97a
eroots <eroots087@gmail.com>
2024-02-09 19:32:33 -0500

    Setting RBF degree to 0 to more closely match NN interp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='pyMT',
-      version='0.104',
+      version='0.105',
       description='Tools and GUIs for MT data analysis and modelling',
       url='http://github.com/eroots/pyMT',
       author='Eric Roots',
